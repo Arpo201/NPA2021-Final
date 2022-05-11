28cba233098041e34d0f3ad08fd65dfb5a723c5b
Arpo201 <arpo.s201@gmail.com>
2022-05-11 10:24:08 +0000

    update

--- 62070174-bot.py
+++ 62070174-bot.py
@@ -5,9 +5,10 @@ requests.packages.urllib3.disable_warnings()
 
 
 def main():
-    access_token = 'MzBmNWJkODItZDhmNy00Zjg4LTg0YmMtN2Y4ZjcxM2I3MThhMzYwYzljOGYtOTQ2_P0A1_4a252141-f787-4173-a4c9-bde69c553a24'
+    access_token = 'YjgzYjMzM2UtZTYwMS00ZDg0LTkwMjUtMWU5ZjZlYTZmNTg4ZDUzMTNhMWItY2Rl_P0A1_4a252141-f787-4173-a4c9-bde69c553a24'
     room_id = 'Y2lzY29zcGFyazovL3VzL1JPT00vNjUwODkzMjAtY2QxOS0xMWVjLWE1NGUtNGQ2MmNhMWM4YmVl'
-    verifyWebexMsg(getIntStatus(), access_token, room_id)
+    intStatus = getIntStatus()
+    verifyWebexMsg(intStatus, access_token, room_id)
 
 def getIntStatus():
     ####################################
@@ -25,10 +26,9 @@ def getIntStatus():
 
     if 200 == resp.status_code:
         response_json = resp.json()
-        int_status = "Loopback62070174 - Operational status is " + response_json['ietf-interfaces:interface']['oper-status']
-    else:
-        int_status = "Loopback62070174 - Operational status is down"
-    return int_status
+        if "up" == response_json['ietf-interfaces:interface']['oper-status']:
+            return 1
+    return 0
 
 def verifyWebexMsg(int_status, access_token, room_id):
     ####################################
@@ -44,17 +44,64 @@ def verifyWebexMsg(int_status, access_token, room_id):
     }
     res = requests.get(url, headers=headers, params=params)
     response_json = res.json()
-
+    intStatus = "Loopback62070174 - Operational status is down"
+    if int_status:
+        intStatus = "Loopback62070174 - Operational status is up"
+    else:
+        intStatus = "Loopback62070174 - Operational status is down"
     if len(response_json["items"]) != 0:
         if response_json["items"][0]["text"] == "62070174":
             params = {
                 'roomId': room_id,
-                'text': int_status
+                'text': intStatus
             }
             res = requests.post(url, headers=headers, json=params)
+            if not int_status:
+                fixInt()
+                int_status = getIntStatus()
+                if int_status:
+                    intStatus = "Enable Loopback62070174 - Now the Operational status is up again"
+                else:
+                    intStatus = "Enable Loopback62070174 - Now the Operational status is still down"
+                params = {
+                    'roomId': room_id,
+                    'text': intStatus
+                }
+                res = requests.post(url, headers=headers, json=params)
         print("Received message: " + response_json["items"][0]["text"])
+
+def fixInt():
+
+    api_url = "https://10.0.15.111/restconf/data/ietf-interfaces:interfaces/interface=Loopback62070174"
+
+    headers = {"Accept": "application/yang-data+json",
+            "Content-type": "application/yang-data+json"
+            }
+    basicauth = ("admin", "cisco")
+
+    yangConfig = {
+        "ietf-interfaces:interface": {
+            "name": "Loopback62070174",
+            "description": "This is Loopback62070174",
+            "type": "iana-if-type:softwareLoopback",
+            "enabled": True,
+            "ietf-ip:ipv4": {
+                "address": [
+                    {
+                        "ip": "192.168.1.1",
+                        "netmask": "255.255.255.0"
+                    }
+                ]
+            },
+            "ietf-ip:ipv6": {}
+        }
+    }
+
+    resp = requests.put(api_url, data=json.dumps(yangConfig),
+                        auth=basicauth, headers=headers, verify=False)
 
 
 while True:
     main()
     time.sleep(1)
+# print(getIntStatus())
